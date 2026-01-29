659ce0dbd68683378f22a04b16f07c1e9bb07cf3
Thanmaya H Rao <148380768+ThanmayaHRao@users.noreply.github.com>
2026-01-29 21:54:42 +0530

    Time: 6 ms (99.52%) | Memory: 47.2 MB (98.66%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = 0;
+        for (int pile : piles) {
+            high = Math.max(high, pile);
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canEat(piles, h, mid)) {
+                high = mid - 1;   
+            } else {
+                low = mid + 1;   
+            }
+        }
+        return low;
+    }
+    private boolean canEat(int[] piles, int h, int k) {
+        long hours = 0;
+
+        for (int pile : piles) {
+            hours += (pile + k - 1) / k;  
+        }
+        return hours <= h;
+    }
+}
