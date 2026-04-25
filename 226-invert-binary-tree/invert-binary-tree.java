13715bc5ae43a0ca5728c6b59759db9e36a52fb8
Thanmaya H Rao <148380768+ThanmayaHRao@users.noreply.github.com>
2026-04-25 15:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (28.43%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -15,23 +15,23 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null){
+        if(root == null ){
             return null;
+            
         }
-        final Queue<TreeNode> queue = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
+
         while(!queue.isEmpty()){
-           final TreeNode node =queue.poll();
+           final TreeNode node = queue.poll();
 
            TreeNode temp = node.left;
            node.left = node.right;
            node.right = temp;
-         
-          if(node.left != null) queue.add(node.left);
-          if(node.right != null) queue.add(node.right);
 
-        }
-        return root;
-
+           if(node.left!=null) queue.add(node.left);
+            if(node.right!=null) queue.add(node.right);
     }
+    return root;
+} 
 }
